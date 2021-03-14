05d820158751e0bc2443047b1e326412053dfed5
Prathamesh Ramane <ryder.coding@gmail.com>
2021-03-14 11:39:25 +0530

    Dependency Checked

--- venv3/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ryder/Projects/Web development/Out Of The Box/olympiad/venv3/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
